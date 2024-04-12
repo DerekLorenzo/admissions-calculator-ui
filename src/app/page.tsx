4902135abc062a63fe3a0059b258d84a3b183973
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2024-04-12 19:59:05 -0400

    Added simple ui elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import Multiselect from 'multiselect-react-dropdown';
 import {useState} from "react";
+import Header from "@/app/common/header";
+import Footer from "@/app/common/footer";
 
 export default function Home() {
     const [colleges, setColleges] = useState<string[]>([]);
@@ -49,20 +51,57 @@ export default function Home() {
     }
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Multiselect
-                className="multiselect"
-                isObject={false}
-                options={colleges}
-                onKeyPressFn={function noRefCheck() {}}
-                onRemove={handleChangeMultiselect}
-                onSelect={handleChangeMultiselect}
-            />
-            <button onClick={getCalculatedRate}>Get Calculated Rate</button>
-            {calculatedRate > 0 ?
+        <main className="flex-initial max-w-full min-h-screen flex-col justify-between p-24">
             <div>
-                <span>Calculated Rate: {calculatedRate}%</span>
-            </div> : null}
+                <Header/>
+            </div>
+            <div className="flex justify-center">
+                <div className="md:w-1/2 sm:w-3/4">
+                    <Multiselect
+                        style={{chips: {background: "rgb(2 132 199)"}}}
+                        className="multiselect"
+                        isObject={false}
+                        options={colleges}
+                        onKeyPressFn={function noRefCheck() {
+                        }}
+                        onRemove={handleChangeMultiselect}
+                        onSelect={handleChangeMultiselect}
+                    />
+                </div>
+            </div>
+            <div className="flex flex-col items-center pt-4">
+                <button
+                    className="bg-sky-600 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded-full"
+                    onClick={getCalculatedRate}
+                >
+                    Get Calculated Rate
+                </button>
+            </div>
+            {calculatedRate > 0 ?
+                <div className="flex justify-center col-span-6 sm:col-span-6 md:col-span-6 lg:col-span-2 xl:col-span-2">
+                    <div
+                        className="bg-white shadow-xl border border-solid rounded-lg px-4 py-6 mx-4 my-4 md:w-1/3 sm:w-1/2">
+                        <div className="text-nowrap justify-center text-center">
+                            <div>
+                            <span>
+                                Estimated Acceptance Rate:
+                            </span>
+                            </div>
+                            <div className="pt-2">
+                            <span className="font-bold">
+                                {calculatedRate + "%"}
+                            </span>
+                            </div>
+                        </div>
+                        <div className="flex mx-auto bg-gray-200 rounded-md text-balance text-center py-4 my-2">
+                            Note: This calculation is not a guarantee of admission but rather an estimation of the
+                            likelihood of acceptance for a typical candidate based on historical acceptance rates.
+                        </div>
+                    </div>
+                </div> : null}
+            <div className="flex justify-center">
+                <Footer/>
+            </div>
         </main>
     );
 }
