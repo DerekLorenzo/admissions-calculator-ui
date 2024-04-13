7e3cf6aa82ae6ac2f71c0396bbe5136c57794333
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2024-04-13 00:27:32 -0400

    minor style update for light mode

--- src/app/common/header.tsx
+++ src/app/common/header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <header className="bg-white dark:bg-sky-600 rounded-full">
+        <header className="bg-white dark:bg-sky-600 border-b-2 dark:border-b-0 border-solid border-gray-400 rounded-full">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
